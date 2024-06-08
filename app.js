296578123d6eb4a64f667632a1d95c5559db03fe
MateuszBieda <mateusbieda@gmail.com>
2024-06-08 20:24:07 +0200

    adding mongoose

--- app.js
+++ app.js
@@ -1,25 +1,46 @@
-const express = require('express')
-const logger = require('morgan')
-const cors = require('cors')
+const express = require("express");
+const logger = require("morgan");
+const cors = require("cors");
+// const mongoose = require("mongoose");
 
-const contactsRouter = require('./routes/api/contacts')
+// require("dotenv").config();
 
-const app = express()
+const contactsRouter = require("./routes/api/contacts");
 
-const formatsLogger = app.get('env') === 'development' ? 'dev' : 'short'
+const app = express();
 
-app.use(logger(formatsLogger))
-app.use(cors())
-app.use(express.json())
+const formatsLogger = app.get("env") === "development" ? "dev" : "short";
 
-app.use('/api/contacts', contactsRouter)
+// const PORT = process.env.PORT || 3000
+// const uriDb = process.env.DB_HOST
+
+// const connection = mongoose.connect(uriDb, {
+//   useUnifiedTopology: true,
+//   useFindAndModify: false,
+// });
+
+// connection
+//   .then(() => {
+//     app.listen(PORT, function () {
+//       console.log(`Server running. Use our API on port: ${PORT}`)
+//     })
+//   })
+//   .catch((err) =>
+//     console.log(`Server not running. Error message: ${err.message}`),
+//   )
+
+app.use(logger(formatsLogger));
+app.use(cors());
+app.use(express.json());
+
+app.use("/api/contacts", contactsRouter);
 
 app.use((req, res) => {
-  res.status(404).json({ message: 'Not found' })
-})
+  res.status(404).json({ message: "Not found" });
+});
 
 app.use((err, req, res, next) => {
-  res.status(500).json({ message: err.message })
-})
+  res.status(500).json({ message: err.message });
+});
 
-module.exports = app
+module.exports = app;
