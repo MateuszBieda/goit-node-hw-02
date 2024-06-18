86bcf779b88a2607fec3e5d18c7aed8550752da9
MateuszBieda <mateusbieda@gmail.com>
2024-06-18 23:22:20 +0200

    adding user.js

--- models/contacts.model.js
+++ models/contacts.model.js
@@ -18,22 +18,3 @@ const Contact = mongoose.model("contact", contact);
 
 module.exports = Contact;
 
-// // const fs = require('fs/promises')
-
-// const listContacts = async () => {}
-
-// const getContactById = async (contactId) => {}
-
-// const removeContact = async (contactId) => {}
-
-// const addContact = async (body) => {}
-
-// const updateContact = async (contactId, body) => {}
-
-// module.exports = {
-//   listContacts,
-//   getContactById,
-//   removeContact,
-//   addContact,
-//   updateContact,
-// }
