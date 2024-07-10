909dcbc4c7708441e21f66abb409bb2d17652b12
MateuszBieda <mateusbieda@gmail.com>
2024-07-11 00:58:24 +0200

    multer diskStorage update

--- routes/routes/auth.routes.js
+++ routes/routes/auth.routes.js
@@ -1,12 +1,15 @@
 const express = require("express");
 const router = express.Router();
 
+
 const authController = require("../../controllers/auth.controller");
 const auth = require("../../middlewares/auth");
+
 router.post("/users/login", authController.login);
 router.post("/users/signup", authController.signup);
 router.get("/users/logout", auth, authController.logout);
 router.get("/users/current", auth, authController.getCurrentUser);
+router.patch("users/avatars", )
 // router.get("/avatars/:imgPath", (req,res)=>{
 //     const {imgPath} = req.params;
 // })
