26a098e13f80d4c2bb954099212cf96a240d377e
MateuszBieda <mateusbieda@gmail.com>
2024-07-16 00:00:05 +0200

    update User model

--- app.js
+++ app.js
@@ -17,6 +17,29 @@ const connection = mongoose.connect(process.env.DATABASE_URL, {
 app.use(express.json());
 app.use(contactsRouter, authRouter);
 
+// ===================================================
+// using Twilio SendGrid's v3 Node.js Library
+// https://github.com/sendgrid/sendgrid-nodejs
+
+const sgMail = require("@sendgrid/mail");
+sgMail.setApiKey(process.env.SENDGRID_API_KEY);
+const msg = {
+  to: "mateusbied@wp.pl", // Change to your recipient
+  from: "mateusbied@wp.pl", // Change to your verified sender
+  subject: "Sending with SendGrid is Fun",
+  text: "and easy to do anywhere, even with Node.js",
+  html: "<strong>and easy to do anywhere, even with Node.js</strong>",
+};
+sgMail
+  .send(msg)
+  .then(() => {
+    console.log("Email sent");
+  })
+  .catch((error) => {
+    console.error(error);
+  });
+
+// =======================================
 app.use(express.static("public"));
 app.use("/avatars", express.static("avatars"));
 
